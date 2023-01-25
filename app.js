479fceb9de5f5e0b1593bcf762e910539a4a0fac
delacruzralph <ralphlewisdelacruz@gmail.com>
2023-01-24 22:24:52 -0500

    added js to check input values

--- app.js
+++ app.js
@@ -1,1 +1,32 @@
-console.log("hello");
+let currency = document.getElementById('currency').value;
+let payRate = document.getElementById('pay-rate').value;
+let perUnit = document.getElementById('per').value;
+console.log(currency);
+console.log(payRate);
+console.log(perUnit);
+
+function handleChange(e) {
+  let value = e.target.value;
+  let targetId = e.target.getAttribute('id');
+
+  updateValue(value, targetId);
+}
+
+function updateValue(value, targetId) {
+  switch(targetId) {
+    case 'currency': 
+      currency = value;
+      break;
+    case 'pay-rate':
+      payRate = value;
+      break;
+    case 'per':
+      perUnit = value;
+      break;
+  }
+}
+
+function startCounter() {
+  console.log(currency, payRate, perUnit);
+  console.log("Start");
+}
