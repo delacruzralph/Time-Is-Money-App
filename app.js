b10cd3244961e2f66571a403bade030469bd0852
delacruzralph <ralphlewisdelacruz@gmail.com>
2023-01-25 19:20:12 -0500

    completed wage counter functionality!

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 /* TODO
-  - Timer 
   - Wage Counter
   - Pause Counter
   - Stop Counter
@@ -10,14 +9,16 @@
 
 let currency = document.getElementById('currency').value;
 let payRate = document.getElementById('pay-rate').value;
-let perUnit = document.getElementById('per').value;
 
 let elapsedTime;
 let elapsedTimeSec;
 let elapsedTimeMin;
 let elapsedTimeHr;
-let intervalId;
+let timerIntervalId;
+let counterIntervalId;
 let timerText;
+let payText;
+let payEarned;
 
 function handleChange(e) {
   let value = e.target.value;
@@ -34,40 +35,59 @@ function updateValue(value, targetId) {
     case 'pay-rate':
       payRate = value;
       break;
-    case 'per':
-      perUnit = value;
+  }
+
+  switch (currency) {
+    case "USD":
+      symbol = "$"
+      break;
+    case "EUR":
+      symbol = "€"
+      break;
+    case "JPY":
+      symbol = "¥"
+      break;
+    case "GBP":
+      symbol = "£"
       break;
   }
 }
 
 function startTimer() {
-  timerText = document.getElementById("time-text");
-  console.log(timerText);
   const startTime = Date.now();
-  intervalId = setInterval(function () {
+  timerIntervalId = setInterval(function () {
     elapsedTime = Date.now() - startTime;
+  }, 10);
+}
 
-    console.log(elapsedTime);
-    // timerText.innerText = elapsedTimeSec;
+function startPayCounter() {
+  counterIntervalId = setInterval(function () {
+    payEarned = (parseFloat((payRate / 60 / 60) * (elapsedTime / 1000))).toFixed(2) + '';
   }, 10);
 }
 
 // Controller
 
 function startCounter() {
-  console.log(currency, payRate, perUnit);
-  console.log("Start");
+  document.getElementById('pay-rate').disabled = true;
   startTimer();
   renderTimer();
+  startPayCounter();
+  renderPayCounter();
 }
 
 function stopCounter() {
-  clearInterval(intervalId);
+  document.getElementById('pay-rate').disabled = false;
+  clearInterval(timerIntervalId);
+  clearInterval(counterIntervalId);
+  payEarned = parseFloat(0).toFixed(2);
+  elapsedTime = 0;
 }
 
 // View
 
 function renderTimer() {
+  timerText = document.getElementById("time-text");
   setInterval(function () {
     elapsedTimeHr = Math.floor(elapsedTime / (1000 * 60 * 60)) + '';
     elapsedTimeMin = Math.floor((elapsedTime % (1000 * 60 * 60)) / (1000 * 60)) + '';
@@ -76,24 +96,10 @@ function renderTimer() {
   }, 10);
 }
 
-// ChatGPT
-// var startBtn = document.getElementById("start-btn");
-// var stopBtn = document.getElementById("stop-btn");
-// var timerDiv = document.getElementById("time-worked");
-// var startTime;
-// var intervalId;
+function renderPayCounter() {
+  payText = document.getElementById("pay-text");
 
-// startBtn.addEventListener("click", function () {
-//   startTime = Date.now();
-//   intervalId = setInterval(function () {
-//     var elapsedTime = Date.now() - startTime;
-//     var hours = Math.floor(elapsedTime / (1000 * 60 * 60));
-//     var minutes = Math.floor((elapsedTime % (1000 * 60 * 60)) / (1000 * 60));
-//     var seconds = Math.floor((elapsedTime % (1000 * 60)) / 1000);
-//     timerDiv.innerHTML = hours + " hours " + minutes + " minutes " + seconds + " seconds";
-//   }, 10);
-// });
-
-// stopBtn.addEventListener("click", function () {
-//   clearInterval(intervalId);
-// });
+  setInterval(function () {
+    payText.innerText = symbol + payEarned;
+  }, 10);
+}
